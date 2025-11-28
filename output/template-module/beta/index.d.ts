d1fec1eee7425f0aad8e7659c0bfae557a72a2e7
MrSociety404 <fabrice@altab.be>
2025-11-28 16:51:45 +0100

    chore: generate interface

--- output/template-module/beta/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AllowedComponentProps, Component, VNodeProps } from "vue";
-export type ExtractComponentProps<TComponent> = TComponent extends new () => {
-    $props: infer P;
-} ? Omit<P, keyof VNodeProps | keyof AllowedComponentProps> : never;
-export declare const render: {
-    <T extends Component>(component: T, props?: ExtractComponentProps<T>): Promise<string>;
-    (vueFilePath: string, props?: Record<string, unknown>): Promise<string>;
-};
--- src/implementations/email/beta/index.ts
+++ src/implementations/email/beta/index.ts
@@ -1,5 +1,28 @@
-import type { EmailParams, EmailResponse } from "@ajs/email/beta";
+import type {
+  BatchEmailParams,
+  BatchEmailResponse,
+  EmailParams,
+  EmailResponse,
+  ProviderCapabilities,
+  TemplateEmailParams,
+} from "@ajs/email/beta";
 
-export function send(_email: EmailParams): Promise<EmailResponse> {
-  return Promise.resolve({ success: true, message: "Email sent" });
+export function Send(params: EmailParams): Promise<EmailResponse> {
+  return Promise.resolve({ params } as any);
+}
+
+export function SendBatch(
+  params: BatchEmailParams,
+): Promise<BatchEmailResponse> {
+  return Promise.resolve({ params } as any);
+}
+
+export function SendTemplate(
+  params: TemplateEmailParams,
+): Promise<EmailResponse> {
+  return Promise.resolve({ params } as any);
+}
+
+export function GetCapabilities(): Promise<ProviderCapabilities> {
+  return Promise.resolve({} as any);
 }
